49e5362de85c8652a4512888b55838f6a0993570
zipper1005 <rajibprasetya43@gmail.com>
2026-09-21 11:50:34 +0700

    Update bot_loker.py

--- bot_loker.py
+++ bot_loker.py
@@ -94,7 +94,7 @@ def kurasi_dengan_gemini(daftar_loker):
     """
     
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.6-flash",
         contents=prompt
     )
     return response.text
